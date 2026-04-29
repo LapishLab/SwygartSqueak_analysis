ee999036dac8c3d4dd6191063bc3fe0cdd312087
Brain1 <>
2026-04-29 16:16:44 -0400

    plot labels with box instance

--- performance_calculations/plot_box_instance.m
+++ performance_calculations/plot_box_instance.m
@@ -1,4 +1,4 @@
-function plot_box_instance(target_box, other_boxes, audio_file)
+function plot_box_instance(target_box, other_boxes, audio_file, target_label, other_label)
 
 % add callback function to change color limit with scroll wheel
 h = figure(1); clf;
@@ -9,12 +9,18 @@ mid_time = target_box(1) + target_box(3)/2;
 window_size = 0.5;
 time_range = [mid_time-window_size/2, mid_time+window_size/2];
 
+%add check to set time_range(1) if it is negative. Means that the call is
+%extending beyond the start of the file which is impossible so set it to 0
+%in this case
+if time_range(1) < 0
+    time_range(1) = 0;
+end 
 
 plot_spectrum(audio_file, time_range(1), time_range(2));
-plot_boxes(target_box, 'green')
+plot_boxes(target_box, 'green', target_label)
 
 % TODO: restrict other boxes to within this window
-plot_boxes(other_boxes, 'blue')
+plot_boxes(other_boxes, 'blue', other_label)
 
 [~,filename,~] = fileparts(audio_file);
 title(strrep(filename, '_', '\_'))
@@ -22,9 +28,12 @@ title(strrep(filename, '_', '\_'))
 input("hit enter to continue to next USV \n", "s");
 end
 
-function plot_boxes(boxes, color)
+function plot_boxes(boxes, color,label)
     for i=1:height(boxes)
         rectangle('pos', boxes(i,:), EdgeColor=color)
+        
+        text(boxes(i,1),boxes(i,2), label(i), Color=color, VerticalAlignment="top")
+
     end
 end
 
