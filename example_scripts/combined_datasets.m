79ff3ca04fc930cd528248b62455065ac3dfbc2f
Brain1 <>
2026-05-01 15:25:07 -0400

    change score filtering threshold

--- example_scripts/combined_datasets.m
+++ example_scripts/combined_datasets.m
@@ -33,16 +33,16 @@ network = train_detector(training_data, net_path, save_path=net_path);
 validation_audio = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/Prat_Urgency/audio/validation/";
 rap_predictions = fullfile(tempdir, 'rap_predictions');
 batch_detect_calls(validation_audio, rap_predictions, network)
-[rap_score, rap_details] = detection_performance(rap_val_path, rap_predictions);
+[rap_score, rap_details] = detection_performance(rap_val_path, rap_predictions,min_score=0.75);
 
 %% Run predictions on DD validation audio and report performance
 validation_audio = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/scentEtOH_urgencyDD/audio/validation/";
 dd_predictions = fullfile(tempdir, 'dd_predictions');
 batch_detect_calls(validation_audio, dd_predictions, network)
-[dd_score, dd_details] = detection_performance(dd_val_path, dd_predictions);
+[dd_score, dd_details] = detection_performance(dd_val_path, dd_predictions, min_score=0.75);
 
 %% Run predictions on rap validation audio and report performance
 validation_audio = "/home/lapishla/Documents/GitHub/DeepSqueak-Network-Performance/detection/human_curated/scentEtOH_urgencyRAP/Wistar_Urgency/audio/validation/";
 wrap_predictions = fullfile(tempdir, 'wrap_predictions');
 batch_detect_calls(validation_audio, wrap_predictions, network)
-[wrap_score, wrap_details] = detection_performance(wrap_val_path, wrap_predictions);
+[wrap_score, wrap_details] = detection_performance(wrap_val_path, wrap_predictions, min_score=0.75);
